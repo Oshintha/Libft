04dd529be7ae17aa7d29a1b961d94cbcbe598853
Oshintha Sandaruwan <oshinthasandaruwan@gmail.com>
2024-04-22 21:23:30 +0300

    add modified file

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,27 +11,31 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 static size_t   ft_strlen(char const *str)
 {
     size_t i;
 
     i = 0;
-    while(*(str + 1))
+    while(*(str + i))
         i++;
     return (i);
 }
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t dst_len = 0;
+    size_t  dst_len;
+    size_t  total_len;
+    size_t  i;
+
+    dst_len = 0;
     while (dst[dst_len] && dst_len < dstsize)
         dst_len++;
-    
-    size_t total_len = dst_len + ft_strlen(src);
+    total_len = dst_len + ft_strlen(src);
+    printf("Total length: %zu\n", total_len);
     if (dst_len >= dstsize)
-        return total_len;
-    
-    size_t i = 0;
+        return (total_len);
+    i = 0;
     while (src[i] && dst_len + 1 < dstsize)
     {
         dst[dst_len] = src[i];
@@ -39,7 +43,7 @@ size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
         i++;
     }
     dst[dst_len] = '\0';
-    return total_len;
+    return (total_len);
 }
 #include <stdio.h>
 #include <string.h>
@@ -50,8 +54,8 @@ int main()
     char dst1[20] = "The Foragers"; 
     char dst2[20] = "The Foragers"; 
 
-    strlcat(dst1, str, 10); 
-    printf("Original: %s\n", dst1);
+    //strlcat(dst1, str, 10); 
+    //printf("Original: %s\n", dst1);
     ft_strlcat(dst2, str, 10); 
     printf("My version: %s\n", dst2); 
 
