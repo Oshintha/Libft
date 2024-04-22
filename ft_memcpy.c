cb3234ca5d6d96b093dfc4d4e45540ad4e0a20eb
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c2r4p4.hive.fi>
2024-04-22 18:28:31 +0300

    add midified  file

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,40 +6,45 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 21:38:59 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/04/17 21:38:59 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:44:03 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-void *ft_memcpy(void *restrict dest, const void *restrict src, size_t n) {
-    void *dest_pnt = dest;
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*dest1;
+	char	*src1;
+	size_t	i;
 
-    if (!dest || !src)
-        return dest; // Return dest if either dest or src is null
-
-    while (n--) {
-        *(char *)dest++ = *(const char *)src++;
-    }
-    return dest_pnt;
+	i = 0;
+	dest1 = (char *)dest;
+	src1 = (char *)src;
+	while (i < n)
+	{
+		dest1[i] = src1[i];
+		i++;
+	}
+	return (dest);
 }
 
-#include<stdio.h>
+/* #include<stdio.h>
 #include <string.h>
 
-int main() {
-    char *str;
-    char *dest;
-    str = "Hello Hive!";
-    dest = malloc(5 * sizeof(char)); // Allocate memory for dest
-    char *dest_pnt = memcpy(dest, str, 4);
-    dest[4] = '\0'; // Null-terminate the string
-    printf("%s\n", dest);
-    free(dest); 
+int main() 
+{
+	char str[] = "Hello_Hive!";
+	char str1[] = "Hello_Hive!";
 
-    char *dest_pnt1 = ft_memcpy(dest, str, 4);
-    dest[4] = '\0'; // Null-terminate the string
-    printf("%s", dest);
-    free(dest); 
-    return 0;
-}
+	memcpy(str + 3, str, 4 );
+
+	printf("%s\n", str);
+
+
+	ft_memcpy(str1 + 3, str1, 4 );
+
+	printf("%s\n", str1);
+
+	return 0;
+} */
