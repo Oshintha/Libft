0d9ea6174c5c152a92704920db81b74307287937
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c1r6p6.hive.fi>
2024-04-23 18:42:04 +0300

    add ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 16:55:32 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/04/23 18:39:35 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	if (needle[0] == '\0')
+	{
+		return ((char *)(haystack));
+	}
+	i = 0;
+	while (i < len -1)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+			{
+				return ((char *)(haystack + i));
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+/* 
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    char    haystack[] = "Hello Hive!";
+    char    needle[] = "Hive!";
+	printf("my: %s\n", ft_strnstr(haystack, needle, 15));
+	printf("original: %s\n", strnstr(haystack, needle, 15));
+} */
