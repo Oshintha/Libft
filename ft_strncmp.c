4de35223435f1a6196604981bce81aeee106fcb5
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c1r6p6.hive.fi>
2024-04-23 14:42:45 +0300

    add ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 14:11:31 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/04/23 14:41:55 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libc.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && i < n - 1)
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+/* #include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char *s1 = "Hello Hive!";
+	char *s2 = "Hello World!";
+
+	printf("my: %d\n", ft_strncmp(s1, s2, 5));
+	printf("original: %d\n", strncmp(s1, s2, 5));
+	return (0);
+} */
