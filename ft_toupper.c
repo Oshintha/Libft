50100dd65113ffff16a3e1e2348a89123d00f6e7
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c1r6p6.hive.fi>
2024-04-23 12:59:50 +0300

    add modified file

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,30 +6,29 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 21:43:28 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/04/22 21:43:28 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/04/23 12:59:18 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
-    int i;
+	int	i;
 
-    if ('a' <= c && c <= 'z')
-    {
-        i = (c - 32) ;
-        return (i);
-    }
-    else
-    return (c);    
+	if ('a' <= c && c <= 'z')
+	{
+		i = (c - 32);
+		return (i);
+	}
+	else
+		return (c);
 }
-
-#include <stdio.h>
+/* #include <stdio.h>
 #include <ctype.h>
 
 int main()
 {
-    int i = 'A';
-    printf("my: %d\n", ft_toupper(i));
-    printf("original: %d\n", toupper(i));
-    return (0);
-}
+	int i = 'o';
+	printf("my: %d\n", ft_toupper(i));
+	printf("original: %d\n", toupper(i));
+	return (0);
+} */
