62a295dbc0accf7b724638e6283be198e0307b6e
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c1r6p6.hive.fi>
2024-04-23 19:30:30 +0300

    add ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:42:56 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/04/23 19:17:36 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/04/23 19:29:52 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,8 @@ int	ft_atoi(const char *str)
 	j = 1;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	while (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			j *= -1;
-		str++;
-	}
+	if (*str == '-')
+		j *= -1;
 	while (*str >= '0' && *str <= '9')
 	{
 		i = i * 10 + *str - '0';
@@ -32,14 +28,12 @@ int	ft_atoi(const char *str)
 	}
 	return (i * j);
 }
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-  char str1[] = "---+--+1234ab567";
+  char str1[] = "--1234ab567";
   printf("Orginal %s:\n", str1);
   printf("my put %d:\n", ft_atoi(str1));
   printf("original %d:\n", atoi(str1));
