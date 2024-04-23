4e681fc449699d3e3acf1c5d4382d0b0e2c2c5c0
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c1r6p6.hive.fi>
2024-04-23 14:01:53 +0300

    add modified file

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,35 +6,35 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:29:15 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/04/23 10:29:15 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:58:17 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-            return ((char*)s);
-            i++;
-    }
-    if ( c == '\0')
-        return ((char*) s);
-    return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *) &s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *) &s[i]);
+	return (0);
 }
-
+/* 
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-    const char str[] = "Hello Hive!";
-    int c = '\0';
-    
-    printf("my: %d\n", ft_strchr(str, c));
-    printf("original: %d\n", strchr(str, c));
-    return (0);
-}
+	const char str[] = "Hello Hive!";
+	int c = '\0';
+	
+	printf("my: %s\n", ft_strchr(str, c));
+	printf("original: %s\n", strchr(str, c));
+	return (0);
+} */
