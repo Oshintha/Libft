54d07072bcc36d9e741472194dd20adabed87046
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c2r4p5.hive.fi>
2024-04-25 17:18:28 +0300

    add ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,61 +6,41 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 09:12:07 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/04/25 09:12:07 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:17:39 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
-static int ft_strlen(char const *s)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    int i;
+	int		len;
+	char	*str;
+	char	*new;
 
-    i = 0;
-    while (s[i])
-    {
-        i++;
-    }
-    return (i);
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(int) * (len + 1));
+	if (!(str))
+		return (NULL);
+	new = str;
+	while (*s1)
+		*str++ = *s1++;
+	while (*s2)
+		*str++ = *s2++;
+	*str = '\0';
+	return (new);
 }
-
-char *ft_strjoin(char const *s1, char const *s2)
-{
-    int i;
-    int len1;
-    int total_len;
-    char *str;
-
-    len1 = ft_strlen(s1);
-    total_len = len1 + ft_strlen(s2);
-
-    str = (char *)malloc(sizeof(char) * (total_len));
-    if (!(str))
-        return (NULL);
-    i = 0;
-    while (i < len1)
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    i = 0;
-    while (len1 < total_len )
-    {
-        str[len1] = s2[i];
-        i++;
-        len1++;
-    }
-    return (str);
-}
-
+/* 
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
-    char *s1 = "Hello ";
-    char *s2 = "Hive!";
-    printf("my: %s:", ft_strjoin(s1, s2));
-    //printf("original: %s", strjoin(s1, s2));
-    return (0);
-}
+	char *s1 = "Hello_";
+	char *s2 = "Hive!";
+	printf("my:%s:\n", ft_strjoin(s1, s2));
+	//printf("original: %s", strjoin(s1, s2));
+	return (0);
+} */
