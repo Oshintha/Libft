bd02b2cb4f774bc1cc56a63d4508498c7318be43
Oshintha Sandaruwan <155742370+Oshintha@users.noreply.github.com>
2024-04-25 11:44:04 +0300

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,23 +12,28 @@
 
 int	ft_atoi(const char *str)
 {
-	int	result;
-	int	sign;
+	long long int	result;
+	long long int	check;
+	int				sign;
 
 	result = 0 ;
 	sign = 1;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str == '-')
+	if (*str == '-' || *str == '+')
 	{
-		sign = -1;
+		if (*str == '-')
+			sign = -1;
 		str++;
 	}
-	if (*str == '+')
-		str++;
-	while ( '0' <= *str && *str <= '9')
+	while (*str && ('0' <= *str && *str <= '9'))
 	{
+		check = result;
 		result = result * 10 + *str - '0';
+		if (result / 10 > check && sign < 0)
+			return (0);
+		if (result / 10 < check && sign > 0)
+			return (-1);
 		str++;
 	}
 	return (result * sign);
@@ -38,16 +43,18 @@ int	ft_atoi(const char *str)
 
 int main()
 {
-  char str1[] = "+ 	-1234ab567";
+  char str1[] = " 	-12345448984564686536465ab567";
   printf("Orginal string: %s:\n", str1);
   printf("original atoi: %d:\n", atoi(str1));
-  printf("ft_atoi: %d:\n", ft_atoi(str1));
+  printf("ft_atoi: %d:\n\n", ft_atoi(str1));
   printf("original atoi: %d:\n", atoi("12345"));
-  printf("ft_atoi: %d:\n", ft_atoi("12345"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("12345"));
   printf("original atoi: %d:\n", atoi("-456"));
-  printf("ft_atoi: %d:\n", ft_atoi("-456"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("-456"));
+    printf("original atoi: %d:\n", atoi("+456"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("+456"));
   printf("original atoi: %d:\n", atoi("2147483647"));
-  printf("ft_atoi: %d:\n", ft_atoi("2147483647"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("2147483647"));
   printf("original atoi: %d:\n", atoi("99999999999999999999"));
   printf("ft_atoi: %d:\n", ft_atoi("99999999999999999999"));
   return (0);
