5f1de30782f86819afd8ec498db2aeb88350fec2
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c2r4p5.hive.fi>
2024-04-25 11:38:07 +0300

    add modified ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,35 +6,56 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 18:42:56 by aoshinth          #+#    #+#             */
-/*   Updated: 2024/04/23 19:29:52 by aoshinth         ###   ########.fr       */
+/*   Updated: 2024/04/25 11:37:16 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	j;
+	long long int	result;
+	long long int	check;
+	int				sign;
 
-	i = 0;
-	j = 1;
+	result = 0 ;
+	sign = 1;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str == '-')
-		j *= -1;
-	while (*str >= '0' && *str <= '9')
+	if (*str == '-' || *str == '+')
 	{
-		i = i * 10 + *str - '0';
+		if (*str == '-')
+			sign = -1;
 		str++;
 	}
-	return (i * j);
+	while (*str && ('0' <= *str && *str <= '9'))
+	{
+		check = result;
+		result = result * 10 + *str - '0';
+		if (result / 10 > check && sign < 0)
+			return (0);
+		if (result / 10 < check && sign > 0)
+			return (-1);
+		str++;
+	}
+	return (result * sign);
 }
-#include <stdio.h>
+/* #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-  char str1[] = "--1234ab567";
-  printf("Orginal %s:\n", str1);
-  printf("my put %d:\n", ft_atoi(str1));
-  printf("original %d:\n", atoi(str1));
-}
+  char str1[] = " 	-12345448984564686536465ab567";
+  printf("Orginal string: %s:\n", str1);
+  printf("original atoi: %d:\n", atoi(str1));
+  printf("ft_atoi: %d:\n\n", ft_atoi(str1));
+  printf("original atoi: %d:\n", atoi("12345"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("12345"));
+  printf("original atoi: %d:\n", atoi("-456"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("-456"));
+    printf("original atoi: %d:\n", atoi("+456"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("+456"));
+  printf("original atoi: %d:\n", atoi("2147483647"));
+  printf("ft_atoi: %d:\n\n", ft_atoi("2147483647"));
+  printf("original atoi: %d:\n", atoi("99999999999999999999"));
+  printf("ft_atoi: %d:\n", ft_atoi("99999999999999999999"));
+  return (0);
+} */
