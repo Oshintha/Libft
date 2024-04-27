c329d5c8ce3cf1088091f02127a36e5541b92be0
Oshintha Sandaruwan <oshinthasandaruwan@gmail.com>
2024-04-27 22:13:01 +0300

    add modified libft.h

--- libft.h
+++ libft.h
@@ -18,15 +18,11 @@
 # include <string.h>
 # include <limits.h>
 
-/*                                 STRUCT                                     */
-
 typedef struct s_list
 {
-	void		*content;
+	void			*content;
 	struct s_list	*next;
-}	t_list;
-
-/*                                  CHECK                                     */
+}					t_list;
 
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
@@ -34,8 +30,6 @@ int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
 int	ft_isspace(int c);
-
-/*                                 MEMORY                                     */
 
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t n);
@@ -45,14 +39,10 @@ void	*ft_memcpy(void	*dst, void const *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
 
-/*                                  PRINT                                     */
-
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
-
-/*                                 STRING                                     */
 
 int	    ft_atoi(char const *str);
 char	    *ft_itoa(int n);
@@ -72,8 +62,6 @@ char	    *ft_strtrim(char const *s1, char const *set);
 char	    *ft_substr(char const *s, unsigned int start, size_t len);
 int	    ft_tolower(int c);
 int	    ft_toupper(int c);
-
-/*                                   LIST                                     */
 
 //void	ft_lstadd_back(t_list **lst, t_list *new);
 //void	ft_lstadd_front(t_list **lst, t_list *new);
