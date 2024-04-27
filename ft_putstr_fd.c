cc0f472ea466909cd1ad88058a640520252ed991
Oshintha Sandaruwan <oshinthasandaruwan@gmail.com>
2024-04-27 20:47:17 +0300

    add ft_putstr_fd.c

---
+++ ft_putstr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 20:30:38 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/04/27 20:30:38 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+void ft_putstr_fd(char *s, int fd)
+{
+    write (fd, s, ft_strlen(s));
+}
+
+#include <stdio.h>
+
+int main()
+{
+    char *s = "Hello Hive!";
+    ft_putstr_fd(s, 1);
+    printf("\n");
+    return (0);
+}
