172a8bed4d7e276f08bf03d9cacc9d2bae8565a1
Oshintha Sandaruwan <oshinthasandaruwan@gmail.com>
2024-04-27 20:58:04 +0300

    ft_putendl_fd.c

---
+++ ft_putendl_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 20:49:27 by aoshinth          #+#    #+#             */
+/*   Updated: 2024/04/27 20:49:27 by aoshinth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_putendl_fd(char *s, int fd)
+{
+    if (s)
+    {
+        write (fd, s, ft_strlen(s));
+        write (fd, "\n", 1);
+    }
+}
+
+#include <stdio.h>
+
+int main()
+{
+    char *s = "Hello Hive!";
+    ft_putendl_fd(s, 1);
+    printf("\n");
+    return (0);
+}
