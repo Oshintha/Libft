739c93414813851f84d0c62a215732ae9ee003c0
Oshintha Sandaruwan <oshinthasandaruwan@gmail.com>
2024-05-04 21:45:50 +0300

    add modified code

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -28,3 +28,34 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		temp = temp->next;
 	temp->next = new;
 }
+
+#include <stdio.h>
+
+int main()
+{
+	t_list *lst = NULL;
+	t_list *new1 = ft_lstnew("Hello ");
+	ft_lstadd_back(&lst, new1);
+
+	t_list *new2 = ft_lstnew("Hive!");
+	ft_lstadd_back(&lst, new2);
+
+	// Printing the contents of the list
+	t_list *current = lst;
+	while (current != NULL)
+	{
+		printf("%s", (char *)current->content);
+		current = current->next;
+	}
+
+	// Freeing the memory allocated for the list
+	t_list *tmp;
+	while (lst != NULL)
+	{
+		tmp = lst->next;
+		free(lst);
+		lst = tmp;
+	}
+
+	return 0;
+}
